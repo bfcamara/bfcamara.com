25309db1ad14745c5302c43debc70fbf7783d4b4
bfcamara <bruno.camara@gmail.com>
2019-09-27 17:29:13 +0100

    Fix thumbnails generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960,
-              withWebp: true,
+              withWebp: false,
               ignoreFileExtensions: [],
             }
           },
