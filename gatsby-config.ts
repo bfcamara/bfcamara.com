175b919f71854b00392454fec658afee734833c5
bfcamara <bruno.camara@gmail.com>
2024-02-09 20:27:24 +0000

    rollback config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -109,12 +109,7 @@ export default {
             options: { wrapperStyle: "margin-bottom: 1.0725rem" },
           },
           "gatsby-remark-autolink-headers",
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              showLineNumbers: true
-            }
-          },
+          "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
           "gatsby-remark-external-links",
